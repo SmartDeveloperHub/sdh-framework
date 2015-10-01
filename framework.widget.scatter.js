0cd7cdbb7425e3e32899f0f5daaa52d4db5d94f2
Alejandro Vera <xafilox@gmail.com>
2015-10-01 13:55:42 +0200

    [Widget][Scatter] Added more configuration parameters and some documentation.

--- framework.widget.scatter.js
+++ framework.widget.scatter.js
@@ -21,128 +21,133 @@
 
 (function() {
 
-    var normalizeConfig = function normalizeConfig(configuration) {
-
-        if (configuration == null) {
-            configuration = {};
+    var defaultConfig = {
+        height: {
+            type: ['number'],
+            default: 240
+        },
+        color: {
+            type: ['function'],
+            default: null
+        },
+        size: {
+            type: ['function'],
+            default: null
+        },
+        shape: {
+            type: ['function'],
+            default: null
+        },
+        x: {
+            type: ['function'],
+            default: null
+        },
+        showXAxis: {
+            type: ['boolean'],
+            default: true
+        },
+        xAxisLabel: {
+            type: ['string'],
+            default: null
+        },
+        xAxisTicks: {
+            type: ['number'],
+            default: 2
+        },
+        xAxisFormat: {
+            type: ['function'],
+            default: d3.format('.03f')
+        },
+        y: {
+            type: ['function'],
+            default: null
+        },
+        showYAxis: {
+            type: ['boolean'],
+            default: true
+        },
+        yAxisLabel: {
+            type: ['string'],
+            default: null
+        },
+        yAxisTicks: {
+            type: ['number'],
+            default: 2
+        },
+        yAxisFormat: {
+            type: ['function'],
+            default: d3.format('.03f')
+        },
+        groupBy: {
+            type: ['string'],
+            default: null
+        },
+        showLegend: {
+            type: ['boolean'],
+            default: true
+        },
+        showDistX: {
+            type: ['boolean'],
+            default: false
+        },
+        showDistY: {
+            type: ['boolean'],
+            default: false
+        },
+        labelFormat: {
+            type: ['string'],
+            default: ''
+        },
+        xDomain: {
+            type: [Array],
+            default: null //Dynamically calculated
+        },
+        yDomain: {
+            type: [Array],
+            default: null //Dynamically calculated
+        },
+        pointDomain: {
+            type: [Array],
+            default: null //Dynamically calculated
+        },
+        clipEdge: {
+            type: ['boolean'],
+            default: false
+        },
+        tooltip: {
+            type: ['string', 'function'],
+            default: null
         }
-
-        var defaultConfig = {
-            height: {
-                type: 'number',
-                default: 240
-            },
-            color: {
-                type: 'function',
-                default: null
-            },
-            size: {
-                type: 'function',
-                default: null
-            },
-            shape: {
-                type: 'function',
-                default: null
-            },
-            xAxis: {
-                type: 'function',
-                default: null
-            },
-            xAxisLabel: {
-                type: 'string',
-                default: null
-            },
-            yAxis: {
-                type: 'function',
-                default: null
-            },
-            yAxisLabel: {
-                type: 'string',
-                default: null
-            },
-            groupBy: { //TODO: sobre que arupar las metricas (pid)
-                type: 'string',
-                default: null
-            },
-            showXAxis: {
-                type: 'boolean',
-                default: true
-            },
-            showYAxis: {
-                type: 'boolean',
-                default: true
-            },
-            showLegend: {
-                type: 'boolean',
-                default: true
-            },
-            showDistX: {
-                type: 'boolean',
-                default: false
-            },
-            showDistY: {
-                type: 'boolean',
-                default: false
-            },
-            labelFormat: {
-                type: 'string',
-                default: '%mid%'
-            },
-            xDomain: {
-                type: 'object',
-                default: null //Dynamically calculated
-            },
-            yDomain: {
-                type: 'object',
-                default: null //Dynamically calculated
-            },
-            pointDomain: {
-                type: 'object',
-                default: null //Dynamically calculated
-            },
-            clipEdge: {
-                type: 'boolean',
-                default: false
-            },
-            tooltip: {
-                type: 'string|function',
-                default: null
-            }
-
-        };
-
-        for(var confName in defaultConfig) {
-            var conf = defaultConfig[confName];
-            if (conf['type'].split('|').indexOf(typeof configuration[confName]) === -1) {
-                configuration[confName] = conf['default'];
-            }
-        }
-
-        return configuration;
-
 
     };
 
     /* Scatter constructor
-     *   element: the DOM element that will contain the Scatter
+     *   element: the DOM element that will contain the rangeNv
      *   data: the data id array
      *   contextId: optional.
      *   configuration: additional chart configuration:
      *      {
-     *       ~ donut: boolean - Whether to make a pie graph a donut graph or not.
-     *       ~ growOnHover: boolean - For pie/donut charts, whether to increase slice radius on hover or not.
-     *       ~ cornerRadius: number - For donut charts only, the corner radius (in pixels) of the slices.
-     *       ~ padAngle: number - The percent of the chart that should be spacing between slices.
+     *       ~ height: number - Height of the widget.
+     *       ~ color: function - Function to decide the color of the point.
+     *       ~ size: function - Function to decide the size of the point.
+     *       ~ shape: function - Function to decide the shape of the point. Can be: circle, cross, diamond, square,
+     *          triangle-down, triangle-dup.
+     *       ~ x: function - Function to decide the value of the point in the x axis.
+     *       ~ y: function - Function to decide the value of the point in the y axis.
+     *       ~ groupBy: string - Name of the parameter used to group the requested metrics. For example, 'pid'.
      *       ~ showLegend: boolean - Whether to display the legend or not.
-     *       ~ showLabels: boolean - Show pie/donut chart labels for each slice.
-     *       ~ donutRatio: number - Percent of pie radius to cut out of the middle to make the donut. It is multiplied
-     *         by the outer radius to calculate the inner radius, thus it should be between 0 and 1.
-     *       ~ duration: number - Duration in ms to take when updating chart. For things like bar charts, each bar can
-     *         animate by itself but the total time taken should be this value.
+     *       ~ showXAxis: boolean - Display or hide the X axis.
+     *       ~ showYAxis: boolean - Display or hide the Y axis.
+     *       ~ xAxisLabel: string - Label to display in the x axis.
+     *       ~ yAxisLabel: string - Label to display in the y axis.
+     *       ~ xAxisTicks: number - Number of ticks of the x axis.
+     *       ~ yAxisTicks: number - Number of ticks of the y axis.
+     *       ~ xAxisFormat: function - D3 format for the x axis.
+     *       ~ yAxisFormat: function - D3 format for the y axis.
      *       ~ labelFormat: string - Format string for the labels. Metric parameters can be used as variables by
-     *         surrounding their names with percentages. The metric name can also be accessed with %mid%. For example,
-     *         the following is a valid labelFormat: "User: %uid%".
-     *       ~ labelsOutside: boolean - Whether pie/donut chart labels should be outside the slices instead of inside them.
+     *         surrounding their names with the symbol '¬'. Metrics data can be accessed with _D. For example,
+     *         the following is a valid labelFormat: "Repository: ¬_D.repocommits.info.rid.name¬".
+     *       ~ TODO: complete documentation
      *      }
      */
     var Scatter = function Scatter(element, metrics, contextId, configuration) {
@@ -173,7 +178,7 @@
         framework.widgets.CommonWidget.call(this, false, this.element.get(0));
 
         // Configuration
-        this.configuration = normalizeConfig(configuration);
+        this.configuration = this.normalizeConfig(defaultConfig, configuration);
 
         this.element.append('<svg class="blurable"></svg>');
         this.svg = this.element.children("svg");
@@ -229,7 +234,6 @@
 
         var data = [];
         var result = [];
-        var labelVariable = /%(\w|\.)+%/g; //Regex that matches all the "variables" of the label such as %mid%, %pid%...
 
         // Group metric values by a request param
         for(var resourceId in framework_data) {
@@ -265,8 +269,8 @@
                     {
                         size: this.configuration.size(metricsGroup),
                         shape: this.configuration.shape(metricsGroup),
-                        x: this.configuration.xAxis(metricsGroup),
-                        y: this.configuration.yAxis(metricsGroup),
+                        x: this.configuration.x(metricsGroup),
+                        y: this.configuration.y(metricsGroup),
                         data: metricsGroup
                     }
                 ]
@@ -311,11 +315,15 @@
 
 
             //Axis settings
-            this.chart.xAxis.axisLabel(this.configuration.xAxisLabel).tickFormat(d3.format('.03f')).ticks(2);
-            this.chart.yAxis.axisLabel(this.configuration.yAxisLabel).tickFormat(d3.format('.03f')).ticks(2);
+            this.chart.xAxis
+                .axisLabel(this.configuration.xAxisLabel)
+                .tickFormat(this.configuration.xAxisFormat)
+                .ticks(this.configuration.xAxisTicks);
+            this.chart.yAxis
+                .axisLabel(this.configuration.yAxisLabel)
+                .tickFormat(this.configuration.yAxisFormat)
+                .ticks(this.configuration.yAxisTicks);
 
-            //We want to show shapes other than circles.
-            //this.chart.scatter.onlyCircles(false);
 
             d3.select(this.svg.get(0))
                 .datum(data)
