957ed15d52fd9cab3a6e3eb5a6e840e60c26b52f
Alejandro Vera <xafilox@gmail.com>
2015-10-27 12:25:34 +0100

    [Widget][TimeBar] Improved tooltip using qTip.

--- framework.widget.timebar.js
+++ framework.widget.timebar.js
@@ -29,6 +29,14 @@
         color: {
             type: ['function', 'object'],
             default: function() { return "#000000"; }
+        },
+        tooltip: {
+            type: ['string', 'function'],
+            default: null
+        },
+        legend: {
+            type: [Array],
+            default: []
         }
     };
 
@@ -36,7 +44,15 @@
      *   element: the DOM element that will contain the widget
      *   resources: resources to observe
      *   contexts: list of contexts
-     *   configuration: custom widget configuration.
+     *   configuration: additional chart configuration:
+     *      {
+     *       ~ height: number - Height of the widget.
+     *       ~ color: function - Function to decide the color of the point.
+     *       ~ tooltip: string - Format string for the tooltip. Metric parameters can be used as variables by
+     *         surrounding their names with the symbol '¬'. Metrics data can be accessed with _D. For example,
+     *         the following is a valid labelFormat: "Repository: ¬_D.repocommits.info.rid.name¬".
+     *         You can also access extra elements like ¬_E.value¬ and ¬_E.time¬
+     *      }
      */
     var TimeBar = function TimeBar(element, resources, contexts, configuration) {
 
@@ -59,7 +75,75 @@
 
     TimeBar.prototype.updateData = function(framework_data) {
 
-        var data = getNormalizedData(framework_data);
+        var normalizedData = getNormalizedData.call(this,framework_data);
+
+        paint.call(this, normalizedData, framework_data);
+
+
+    };
+
+    TimeBar.prototype.delete = function() {
+
+        // Stop observing for data changes
+        framework.data.stopObserve(this.observeCallback);
+
+        this.element.remove(".progress");
+
+    };
+
+
+    var getNormalizedData = function getNormalizedData(framework_data) {
+
+        /* //TODO: uncomment for real data
+        var resourceId = Object.keys(framework_data)[0];
+        var resourceUID = Object.keys(framework_data[resourceId])[0];
+        var resource = framework_data[resourceId][resourceUID];
+
+        return {
+            values: resource['data']['values'],
+            interval: resource['data']['interval']
+        };
+        */
+
+
+        /*var values = [0];
+        var changeDiff = 0.01;
+        var change = changeDiff;
+        for(var i = 1; i < 1000; i++) {
+
+            if(change === 0 && Math.random() > 0.95) {
+                if(values[i-1] === 0) {
+                    change = changeDiff;
+                } else {
+                    change = -changeDiff;
+                }
+            }
+
+            values[i] = Math.max(0, Math.min(values[i-1] + change, 1));
+            if(values[i] === 0 || values[i] === 1) {
+                change = 0;
+            }
+        }
+
+        return {
+            values: values,
+            interval: {
+                from: 1445327267726,
+                to: 1445427267726
+            }
+        };*/
+
+        return {
+            values: [0.2, 0.2, 0.2, 1, 0.5, 0.5, 0.7, 0.7, 0.7],
+            interval: {
+                from: 1445327267726,
+                to: 1445427267726
+            }
+        };
+
+    };
+
+    var paint = function(data, framework_data) {
 
         // Remove previous container
         this.element.find(".timebar-container").remove();
@@ -85,11 +169,17 @@
         for(var i = 0; i < data.values.length; ++i) {
             var color = colorFunction(data.values[i]);
             var time = data.interval.from + i * timeIncremental;
-            progress.append($('<div class="progress-bar" title="'+ time +'" ' +'style="width: ' + width + '%; background-color: ' + color + '"></div>')
-                    .tooltip({
-                        html: true
-                    })
-            );
+            var content = null;
+
+            //Configure how the tooltip looks.
+            if(typeof this.configuration.tooltip === 'string') {
+                content = this.replace(this.configuration.tooltip, framework_data, { value: data.values[i], time: time } );
+            } else if(typeof this.configuration.tooltip === 'function') {
+                content = this.configuration.tooltip( { value: data.values[i], time: time } );
+            }
+
+            addBarToProgressBar(progress, width, color, content);
+
         }
 
         //Add labels
@@ -100,18 +190,41 @@
                 .css('position', 'absolute')
                 .css('margin', '5px')
                 .css('left', 0)
-            )
+        )
             .append($('<span>' + timeFormat(new Date(data.interval.to)) + '</span>')
                 .css('position', 'absolute')
                 .css('margin', '5px')
                 .css('right', 0)
-            );
+        );
         container.append(xAxis);
 
 
     };
 
-    TimeBar.prototype.delete = function() {
+    var addBarToProgressBar = function (progress, width, color, content) {
+        var bar = $('<div class="progress-bar" style="width: ' + width + '%; background-color: ' + color + '"></div>')
+            .qtip({
+                content: function() {
+                    return content;
+                },
+                show: {
+                    event: 'mouseover'
+                },
+                hide: {
+                    event: 'mouseout'
+                },
+                position: {
+                    my: 'top center',
+                    at: 'bottom center'
+                },
+                style: {
+                    classes: 'qtip-bootstrap',
+                    tip: {
+                        width: 16,
+                        height: 6
+                    }
+                }
+            });
 
         // Stop observing for data changes
         framework.data.stopObserve(this.observeCallback);
@@ -160,7 +273,7 @@
                 from: 1445327267726,
                 to: 1445427267726
             }
-        };
+        progress.append(bar);
 
     };
 
@@ -169,7 +282,7 @@
 
     // AMD compliant
     if ( typeof define === "function" && define.amd) {
-        define( [ /* List of dependencies */ ], function () { return TimeBar; } );
+        define( [ 'jquery-qtip' ], function () { return TimeBar; } );
     }
 
 })();
