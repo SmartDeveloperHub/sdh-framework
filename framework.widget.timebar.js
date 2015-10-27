b8cde2593097d338c7c957a853e9db9b9a722fde
Alejandro Vera <xafilox@gmail.com>
2015-10-27 12:26:47 +0100

    [Widget][TimeBar] Added nvd3 legend.

--- framework.widget.timebar.js
+++ framework.widget.timebar.js
@@ -149,8 +149,12 @@
         this.element.find(".timebar-container").remove();
 
         // Add new bar
-        this.element.append('<div class="timebar-container"><div class="progress" style="height: ' + this.configuration.height + 'px"></div></div>');
+        this.element.append('<div class="timebar-container">' +
+                '<svg class="legend nvd3"></svg>' +
+                '<div class="progress" style="height: ' + this.configuration.height + 'px"></div>' +
+            '</div>');
         var progress = this.element.find(".progress");
+
 
         // Select the color function (an user given function or an object based function)
         var colorFunction = null;
@@ -161,6 +165,40 @@
                 return this.configuration.color[String(val)];
             }.bind(this);
         }
+
+
+        // Create legend
+        if(this.configuration.legend != null && this.configuration.legend.length > 1) {
+
+            var legendSvg = this.element.find("svg.legend");
+            legendSvg.css('height', '25px');
+
+            // Calculate the color for each of the labels in the legend
+            var colors = [];
+            var keys = [];
+            for(var i = 0; i < this.configuration.legend.length; i++) {
+                colors.push(colorFunction( i / (this.configuration.legend.length - 1)));
+                keys.push({ key: this.configuration.legend[i] });
+            }
+
+            var legend = nv.models.legend()
+                .color(colors)
+                .rightAlign(true)
+                .width(this.element.width());
+
+            //Update width on window resize
+            nv.utils.windowResize(function() {
+                legend.width(this.element.width());
+                d3.select(legendSvg.get(0))
+                    .call(legend);
+            }.bind(this));
+
+            d3.select(legendSvg.get(0))
+                .datum(keys)
+                .call(legend);
+
+        }
+
 
         var width = 100 / data.values.length;
         var timeIncremental = (data.interval.to - data.interval.from) / data.values.length;
@@ -226,55 +264,7 @@
                 }
             });
 
-        // Stop observing for data changes
-        framework.data.stopObserve(this.observeCallback);
-
-        this.element.remove(".progress");
-
-    };
-
-
-    var getNormalizedData = function getNormalizedData(framework_data) {
-
-        /* //TODO: uncomment for real data
-        var resourceId = Object.keys(framework_data)[0];
-        var resourceUID = Object.keys(framework_data[resourceId])[0];
-        var resource = framework_data[resourceId][resourceUID];
-
-        return {
-            values: resource['data']['values'],
-            interval: resource['data']['interval']
-        };
-        */
-
-
-        var values = [0];
-        var changeDiff = 0.01;
-        var change = changeDiff;
-        for(var i = 1; i < 1000; i++) {
-
-            if(change === 0 && Math.random() > 0.95) {
-                if(values[i-1] === 0) {
-                    change = changeDiff;
-                } else {
-                    change = -changeDiff;
-                }
-            }
-
-            values[i] = Math.max(0, Math.min(values[i-1] + change, 1));
-            if(values[i] === 0 || values[i] === 1) {
-                change = 0;
-            }
-        }
-
-        return {
-            values: values,
-            interval: {
-                from: 1445327267726,
-                to: 1445427267726
-            }
         progress.append(bar);
-
     };
 
     // Register the widget in the framework
